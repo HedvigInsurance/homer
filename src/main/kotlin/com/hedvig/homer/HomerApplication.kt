09c5df846189e0dab47d69429a641661124578e9
meletios <meletios@hedvig.com>
2020-01-16 10:49:07 +0100

    skeleton

---
+++ src/main/kotlin/com/hedvig/homer/HomerApplication.kt
@@ -0,0 +1,11 @@
+package com.hedvig.homer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HomerApplication
+
+fun main(args: Array<String>) {
+	runApplication<HomerApplication>(*args)
+}
